1fe860421e0d35f3a9c71556a50c0f5671305b04
NineTailed <ninetailed322@gmail.com>
2024-05-20 00:18:23 +0700

    Added a partially completed second task

--- coder.py
+++ coder.py
@@ -1,3 +1,8 @@
+from math import log
+
+# Global var init
+bin_code_comb = []
+
 def dec_num_to_bin_list(num):
 	return list(bin(num))[2:]
 
@@ -25,6 +30,135 @@ def xor_multiple_rows(matrix):
 		buffer_row = xor(buffer_row, matrix[i])
 	
 	return buffer_row
+
+def recursive_vertor_division_shanfano(vector):
+    if len(vector) == 1:
+        return vector
+    
+    counter = 0
+    sum_elems = 0
+    sum_1 = 0
+    sum_2 = 0
+    vector_match = 0
+
+    for i in range(0, len(vector)):
+        vector_match = vector_match + vector[i][0]
+    vector_match /= 2
+    
+    while True:
+        sum_elems += vector[counter][0]
+
+        if sum_elems >= vector_match:
+            sum_1 = recursive_vertor_division_shanfano(vector[:counter + 1])
+            sum_2 = recursive_vertor_division_shanfano(vector[counter + 1:])
+            break
+
+        counter += 1
+
+    for item in sum_1:
+        bin_code_comb[item[1]].append(1)
+    for item in sum_2:
+        bin_code_comb[item[1]].append(0)
+    
+    return sum_1 + sum_2
+
+def shannon_fano(vector, sys_base = 3):
+    vector.sort()
+    vector.reverse()
+
+    vector_new = []
+    for i in range(0, len(vector)):
+        vector_new.append([vector[i], i])
+    vector = vector_new
+
+    global bin_code_comb
+    bin_code_comb = []
+    for i in range(len(vector)):
+        bin_code_comb.append([])
+
+    recursive_vertor_division_shanfano(vector)
+
+    for i in range(0, len(bin_code_comb)):
+        bin_code_comb[i] = bin_code_comb[i][::-1]
+        print(vector[i][0], bin_code_comb[i])
+        
+def huffman(vector, sys_base = 3):
+    vector_prim = vector.copy()
+    vector.sort()
+    vector.reverse()
+
+    vector_new = []
+    for i in range(0, len(vector)):
+        vector_new.append([vector[i], i])
+    vector = vector_new
+
+    global bin_code_comb
+    bin_code_comb = []
+    for i in range(len(vector)):
+        bin_code_comb.append([])
+
+    minimal_1 = []
+    minimal_1_index = 0
+    minimal_2 = []
+    minimal_2_index = 0
+    while len(vector) != 1:
+        minimal_1 = [2, 0]
+        minimal_1_index = 1000
+        minimal_2 = [2, 0]
+        minimal_2_index = 1000
+
+        for i in range(0, len(vector)):
+            if vector[i][0] < minimal_1[0]:
+                minimal_1 = vector[i]
+                minimal_1_index = i
+        vector.pop(minimal_1_index)
+
+        for i in range(0, len(vector)):
+            if vector[i][0] < minimal_2[0]:
+                minimal_2 = vector[i]
+                minimal_2_index = i
+        vector.pop(minimal_2_index)
+
+        if minimal_1[0] == minimal_2[0]:
+            minimal_1, minimal_2 = minimal_2, minimal_1
+
+        min_1_type = type(minimal_1[1])
+        min_2_type = type(minimal_2[1])
+
+        if isinstance(minimal_1[1], list) and isinstance(minimal_2[1], list):
+            vector.append([round(minimal_1[0] + minimal_2[0], 2), minimal_1[1] + minimal_2[1]])
+
+            for item in minimal_1[1]:
+                bin_code_comb[item].append(0)
+            for item in minimal_2[1]:
+                bin_code_comb[item].append(1)
+
+        if isinstance(minimal_1[1], list) and isinstance(minimal_2[1], int):
+            vector.append([round(minimal_1[0] + minimal_2[0], 2), minimal_1[1] + [minimal_2[1]]])
+
+            for item in minimal_1[1]:
+                bin_code_comb[item].append(0)   
+            bin_code_comb[minimal_2[1]].append(1)
+
+        if isinstance(minimal_1[1], int) and isinstance(minimal_2[1], list):
+            vector.append([round(minimal_1[0] + minimal_2[0], 2), [minimal_1[1]] + minimal_2[1]])
+
+            bin_code_comb[minimal_1[1]].append(0)
+            for item in minimal_2[1]:
+                bin_code_comb[item].append(1)
+
+        if isinstance(minimal_1[1], int) and isinstance(minimal_2[1], int):
+            vector.append([round(minimal_1[0] + minimal_2[0], 2), [minimal_1[1]] + [minimal_2[1]]])
+
+            bin_code_comb[minimal_1[1]].append(0)
+            bin_code_comb[minimal_2[1]].append(1)
+
+    avg_len = 0
+    for i in range(0, len(bin_code_comb)):
+        bin_code_comb[i] = bin_code_comb[i][::-1]
+        avg_len += len(bin_code_comb[i])
+        print(vector_prim[i], bin_code_comb[i])
+    print('Average word lentgh is ', round((avg_len/len(vector_prim)), 3))
 
 def first_processing(matrix):
 	# Calculation of n, k, r
@@ -82,7 +216,16 @@ def first_processing(matrix):
 	print(f'qo = {qo}')
 	print(f'qi = {qi}')
 	print(f'w = {w}')
-		
+
+def second_processing(vector):
+	print('Huffman code')
+	huffman(vector)
+	
+	print('Shannon-Fano code')
+	shannon_fano(vector)
+
+	# TODO: Repeat actions with combining two by two
+
 if __name__ == '__main__':
 
 	G = []
@@ -90,16 +233,22 @@ if __name__ == '__main__':
 	with open('Data.txt') as file:
 		file_data = file.readlines()
 		counter = 0
-		for i in range(file_data.index('# 1\n') + 1, len(file_data)):
+		for i in range(file_data.index('# 1\n') + 1, file_data.index('# 2\n')):
 			current_line = list(file_data[i])
 			G.append([])
 			for j in range(0, len(current_line)):
 				if j % 2 == 0:
 					G[counter].append(int(current_line[j]))
 			counter += 1
-		
+        
+		for i in range(file_data.index('# 2\n') + 1, len(file_data)):
+			current_line = list(file_data[i])
+			print(current_line)
+
+			# TODO: Added a partially completed second task
+
 	for item in G:
 		print(item)
 	print()
-			
-	first_processing(G)
+
+	#first_processing(G)
